1d7cef55985590687fdb8097796d9d0fb4de8d0a
Kevin MacKenzie <kevin@homesmoncton.com>
2021-12-06 06:18:17 -0400

    added more shapes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,16 +9,49 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes: false,
+        width,
+        height
     }
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true
-});
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}));
 
-World.add(world, shape);
+//Walls
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, { isStatic: true } ),
+    Bodies.rectangle(400, 600, 800, 40, { isStatic: true } ),
+    Bodies.rectangle(0, 300, 40, 600, { isStatic: true } ),
+    Bodies.rectangle(800, 300, 40, 600, { isStatic: true } )
+];
+
+World.add(world, walls);
+
+// random shapes
+for (let i = 0; i < 20; i++ ) {
+    if (Math.random() < .5) {
+        World.add(
+            world, 
+            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+        );
+    } else {
+        World.add(
+            world, 
+            Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+                render: {
+                    fillStyle: 'red'
+                }
+            } 
+            )
+        );
+    }
+    
+}
+
+
+
