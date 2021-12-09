96ad8fcaba4e307d19dbf1bf46d641325e4a99f8
Kevin MacKenzie <kevin@homesmoncton.com>
2021-12-09 07:33:30 -0400

    added ball movement and the ability to score

--- index.js
+++ index.js
@@ -3,36 +3,39 @@ const {
     Render, 
     Runner, 
     World, 
-    Bodies, 
+    Bodies,
+    Body,
+    Events 
 } = Matter;
 
-const cells = 3;
+const cells = 6;
 const width = 600;
 const height = 600;
 
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0; // disables gravity
 const { world } = engine;
 const render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        wireframes: false,
-        width,
-        height
-    }
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: true,
+    width,
+    height
+  }
 });
-
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
 //Walls
+const border = 2;
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true } ),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true } ),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true } ),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true } )
+    Bodies.rectangle(width / 2, 0, width, border, { isStatic: true } ),
+    Bodies.rectangle(width / 2, height, width, border, { isStatic: true } ),
+    Bodies.rectangle(0, height / 2, border, height, { isStatic: true } ),
+    Bodies.rectangle(width, height / 2, border, height, { isStatic: true } )
 ];
 
 World.add(world, walls);
@@ -122,11 +125,12 @@ horizontals.forEach((row, rowIndex) => {
             return;
         }
         const wall = Bodies.rectangle(
-            (columnIndex * unitLength) + (unitLength / 2),
+            columnIndex * unitLength + (unitLength / 2),
             rowIndex * unitLength + unitLength,
             unitLength,
             10,
             {
+                label: 'wall',
                 isStatic: true
             }
         );
@@ -151,3 +155,59 @@ verticals.forEach((row, rowIndex) => {
         World.add(world, wall);
     });
 });
+
+// added the goal
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * .7,
+    unitLength * .7,
+    {
+        label: 'goal',
+        isStatic: true
+    }
+);
+World.add(world, goal);
+
+// add the ball
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+    label: 'ball'
+  });
+  World.add(world, ball);
+
+// keys to move the ball
+document.addEventListener(`keydown`, event => {
+    const {x, y} = ball.velocity;
+    if (event.keyCode === 87) {
+        Body.setVelocity(ball, {x, y: y - 5});
+    }
+    if (event.keyCode === 68) {
+        Body.setVelocity(ball, {x: x + 5, y });
+    }
+    if (event.keyCode === 83) {
+        Body.setVelocity(ball, {x, y: y + 5 });   
+    }
+    if (event.keyCode === 65) {
+        Body.setVelocity(ball, {x: x - 5, y });    
+    }
+});
+
+// win condition
+
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach(collision => {
+      const labels = ['ball', 'goal'];
+  
+      if (
+        labels.includes(collision.bodyA.label) &&
+        labels.includes(collision.bodyB.label)
+      ) {
+        world.gravity.y = 1;
+        world.bodies.forEach(body => {
+          if (body.label === 'wall') {
+            Body.setStatic(body, false);
+          }
+        });
+      }
+    });
+  });
